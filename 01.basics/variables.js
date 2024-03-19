04538872203f43216b7bb76b086fd746726996de
Ankit Singh <124412433+rajputankit9017@users.noreply.github.com>
2024-03-19 02:50:10 +0000

    variables done

---
+++ 01.basics/variables.js
@@ -0,0 +1,13 @@
+const aki=45
+var hhi="Hello"
+hhi=43
+let accountEmail="ankit@google.com"
+console.log(hhi)
+
+var email="hello@email.com"
+console.log(email)
+
+accountEmail="singh@gmail.com"
+console.log(accountEmail)
+
+console.table([aki,hhi,email,accountEmail])
